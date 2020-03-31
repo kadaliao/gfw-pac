867ffbab34d9ce8882c8e6fd276c580d53832560
Kada Liao <kadaliao@gmail.com>
2020-03-31 23:59:32 +0800

    update template, add bootstrapcdn

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./gfw-pac.py -i gfwlist.txt --user-rule custom-domains.txt -p "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT" -f kada.pac
+./gfw-pac.py -i gfwlist.txt --direct-rule direct-domains.txt --user-rule custom-domains.txt -p "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT" -f kada.pac
